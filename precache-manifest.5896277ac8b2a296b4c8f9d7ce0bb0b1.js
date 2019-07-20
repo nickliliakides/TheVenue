1349ea084c3c9394e0f12e28905879142b877023
Your Name <nickliliakides@gmail.com>
2019-07-21 00:41:05 +0100

    Updates

---
+++ precache-manifest.5896277ac8b2a296b4c8f9d7ce0bb0b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf6f58f4fd28183d2c78cf0fe597398",
+    "url": "/TheVenue/index.html"
+  },
+  {
+    "revision": "cfe83a94b2e9671233ab",
+    "url": "/TheVenue/static/css/main.40349f85.chunk.css"
+  },
+  {
+    "revision": "3a6c419c25aa0793b798",
+    "url": "/TheVenue/static/js/2.41cbd7d6.chunk.js"
+  },
+  {
+    "revision": "cfe83a94b2e9671233ab",
+    "url": "/TheVenue/static/js/main.128a478d.chunk.js"
+  },
+  {
+    "revision": "32792ad071db4117b40b",
+    "url": "/TheVenue/static/js/runtime~main.0076579e.js"
+  },
+  {
+    "revision": "c150624f79c0827df29f94dae21d7b55",
+    "url": "/TheVenue/static/media/Alice-Russell-620x340.c150624f.jpg"
+  },
+  {
+    "revision": "9e03e4e1776e9e85a5d68739ae8a93af",
+    "url": "/TheVenue/static/media/quantic_082211.9e03e4e1.jpg"
+  },
+  {
+    "revision": "85b4f8d74957f9a58729b0315f5e6051",
+    "url": "/TheVenue/static/media/quanticandalice.85b4f8d7.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/TheVenue/precache-manifest.5896277ac8b2a296b4c8f9d7ce0bb0b1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TheVenue/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
